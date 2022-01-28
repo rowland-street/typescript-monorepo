285c359b56714a69306a2921eb365ed96f938e4d
Tom Hodgson <tom@rowlandstreet.com>
2022-01-28 09:18:14 +0000

    use npm workspaces to have different package jsons per pacckage

--- packages/zoo/zoo.ts
+++ packages/zoo/zoo.ts
@@ -1,9 +1,12 @@
 // import Animal from '../animals/index';
 import { Dog, createDog } from '../animals/index';
+import * as _ from "lodash"
 
 export function createZoo(): Array<Dog> {
-    return [
+    return _.concat([
         createDog()
-    ];
+    ], [
+        createDog()
+    ]);
 }
 
